3c0b66963126f8e69635182b8c5ee5baa576670b
Alexandro Tapia <zpaceway@gmail.com>
2024-08-19 00:39:35 -0500

    build

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,14 @@ const commander_1 = require("commander");
 const program = new commander_1.Command();
 program
     .command("proxy")
+    .option("-user, --username <string>", "set the proxy username", "")
+    .option("-pass, --password <string>", "set the proxy password", "")
     .option("-thost, --tunnelHost <string>", "set the tunnel host", "localhost")
     .option("-availability, --minimumAvailability <number>", "the minimum pool availability", "10")
     .action((options) => {
     const parsedOptions = {
+        username: options.username,
+        password: options.password,
         tunnelHost: options.tunnelHost,
         minimumAvailability: parseInt(options.minimumAvailability),
     };
@@ -16,17 +20,30 @@ program
     const proxy = (0, elastic_tools_1.createProxy)(parsedOptions);
     proxy.listen();
 });
-program.command("tunnel").action(() => {
+program
+    .command("tunnel")
+    .option("-user, --username <string>", "set the tunnel username", "")
+    .option("-pass, --password <string>", "set the tunnel password", "")
+    .action((options) => {
+    const parsedOptions = {
+        username: options.username,
+        password: options.password,
+    };
+    console.log("Starting tunnel with the following options:", parsedOptions);
     console.log("Starting elastic tunnel");
-    const tunnel = (0, elastic_tools_1.createTunnel)();
+    const tunnel = (0, elastic_tools_1.createTunnel)(parsedOptions);
     tunnel.listen();
 });
 program
     .command("client")
-    .option("-ccode, --countryCode <number>", "set internal proxy port", "US")
+    .option("-user, --username <string>", "set the client username", "")
+    .option("-pass, --password <string>", "set the client password", "")
     .option("-thost, --tunnelHost <string>", "set the tunnel host", "localhost")
+    .option("-ccode, --countryCode <number>", "set internal proxy port", "US")
     .action((options) => {
     const parsedOptions = {
+        username: options.username,
+        password: options.password,
         countryCode: options.countryCode,
         tunnelHost: options.tunnelHost,
     };
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import { Command } from "commander";
 
 type ToString<T> = { [key in keyof T]: string };
 type CreateClientParams = ToString<Parameters<typeof createClient>[0]>;
+type CreateTunnelParams = ToString<Parameters<typeof createTunnel>[0]>;
 type CreateProxyParams = ToString<Parameters<typeof createProxy>[0]>;
 
 const program = new Command();
 
 program
   .command("proxy")
+  .option("-user, --username <string>", "set the proxy username", "")
+  .option("-pass, --password <string>", "set the proxy password", "")
   .option("-thost, --tunnelHost <string>", "set the tunnel host", "localhost")
   .option(
     "-availability, --minimumAvailability <number>",
@@ -17,6 +20,8 @@ program
   )
   .action((options: CreateProxyParams) => {
     const parsedOptions = {
+      username: options.username,
+      password: options.password,
       tunnelHost: options.tunnelHost,
       minimumAvailability: parseInt(options.minimumAvailability),
     };
@@ -25,18 +30,31 @@ program
     proxy.listen();
   });
 
-program.command("tunnel").action(() => {
-  console.log("Starting elastic tunnel");
-  const tunnel = createTunnel();
-  tunnel.listen();
-});
+program
+  .command("tunnel")
+  .option("-user, --username <string>", "set the tunnel username", "")
+  .option("-pass, --password <string>", "set the tunnel password", "")
+  .action((options: CreateTunnelParams) => {
+    const parsedOptions = {
+      username: options.username,
+      password: options.password,
+    };
+    console.log("Starting tunnel with the following options:", parsedOptions);
+    console.log("Starting elastic tunnel");
+    const tunnel = createTunnel(parsedOptions);
+    tunnel.listen();
+  });
 
 program
   .command("client")
-  .option("-ccode, --countryCode <number>", "set internal proxy port", "US")
+  .option("-user, --username <string>", "set the client username", "")
+  .option("-pass, --password <string>", "set the client password", "")
   .option("-thost, --tunnelHost <string>", "set the tunnel host", "localhost")
+  .option("-ccode, --countryCode <number>", "set internal proxy port", "US")
   .action((options: CreateClientParams) => {
     const parsedOptions = {
+      username: options.username,
+      password: options.password,
       countryCode: options.countryCode as Parameters<
         typeof createClient
       >[0]["countryCode"],
